4a72254afa8fdf5acdecc95f5221a3d92d4f8405
cassidy <cdiamond@cmu.edu>
2025-01-18 22:37:22 +0000

    first steps at python

--- lang.py
+++ lang.py
@@ -1,8 +1,196 @@
 '''
-A program that encodes our rules and constructs syntactically valid sentences with them
+Overall goal of this program: take, as input, a bf function, and using the set syntax rules, construct a grammatical sentence tree. As an additional option, automatically fill in the words with words from a wordbank. The goal should be to construct as short of a mapping as possible.
 
-Start at a TP and apply the only rule we have. At each other level, randomly apply rules to build up our phrases. At the end fill out the heads with words from a word bank.
+* ok, couple of notes. what does "a grammatial sentence tree" look like? Is that a Python object? so then a collection of sentences is an array of these objects?
+* "as short of a mapping as possible" should also note that some rules don't take up any space, or take up less space, as in they may have like 5 rules but only 2 actual words attached. so maybe each "node"/phrase has a certain weight to it, and I start out by manually weighting them with how many words are present. This is a good idea bc then I can modify those weights as the program goes on to, say, avoid repetitive sentences.
 
+* this is a later problem, but when filling out with words -- I have some more niche grammar rules that aren't captured by PSR. for example, it's "You and I", not "I and you". Transitive/ditransitive verbs matter. Some verbs take instrument prepositions phrases, some words don't, etc. But again I'll come back to this later.
 
-god wouldn't it be great if there was some markov-based, simulated annealing way i could stuff the encodings into my rules? we give it a bf string to try and fit itself around. it starts with a random encoding, tries to fit it to a string. we assign it points based on how many times it fails to produce valid sentences. like in MH if we want to pick a new encoding, we randomly change a rule, proceed from there.
+## "naive" idea
+the first idea is to keep track of a "remainder" or "offset" from what the bf program expects the position to be, vs what it actually is. perhaps same thing with `+-`, too. the rules are such that inserting `+-][.,` is *always* optional. There is no sentence that requires the rules corresponding to these operations anywhere. However, nouns and verbs are always required and always associated with some degree of movement/shift.
+
+so. Let's say I want to insert a "+" somewhere. That's an adjective rule, which requires a DP to be placed in. Assume (although it doesn't have to be) that we use the subject DP to insert it, so by the time we get to it it looks like `>>+` or something. No problem, we expect that, and we create another sentence that moves us two spaces *backwards*, before we go for `>>+`, then we end up ultimately in the right place.
+* I guess, same thing with the fact that `+` is actually `+++`, if we want just one then we can insert one adjective, move around, correct, then do the same thing with adverbs, but it'll at least symbols-wise be *faster* to insert an adverb beforehand. So we "wind up" with a sentence of `<<`, then run into `>>--+++` to cancel out into just `+`.
+
+## search idea
+this feels like a *graph traversal* / *search algorithm*. We have a graph that have continuously enter and exit (S: DP VP). At each node each have edges, given by rules, to other nodes (states?), where each edge is associated with an operation for traversal, and probably, some other kind of cost, too.
+
+This feels really fucking abstract but I think `><+-` can be visualized as some kind of multidimensional number system, actually...
+
+## wtf is this graph
+oh god, this thing is fucking infinite?
+* no, not true, but there are loops in it
+
+coming back again, and, I built it. fuck yeah.
+implementation:
+* we start at the node "S", whose left and right children are DP "choices" and VP "choices" respectively. A choice node represents a choice. Here, we can choose whatever we want to proceed down our graph, there are no restrictions on our movement. Other nodes have a left and right child (but, unfortunately, in the case of ditransitive verbs there are actually three; maybe implement this as "left, right, special?").
+* I should have some object that represents this graph, which is fairly easy to construct given my rules. So then, 1), a function that converts a list of rules (given as strings and weights?) into the graph.
+* once I have the graph, I should have an abstract method to traverse it. to do an "in order traversal" -- I mean, recursively you can just move down the left branch, then log/print/whatever the current branch, then go down the right branch, that's easy.
+    * non-recursively, at all non-choice nodes always just go left. if we have have a node that has a right child, add it to a stack. once we get to a leaf/"head" pop off the stack and go back to that node, choose the right child. if our stack is empty we're done.
+
+1) a function that converts a list of rules (and idk, rules are objects with strings and weights?) into the graph.
+
+2) a function to "search" the graph
+    * it's infinite so we have to use BFS ofc
+    * instead of putting costs on the edges we can put costs on the nodes themselves, as long as a node isn't a head/leaf. or put them on edges, it doesn't matter
+    * oh, nuance; when I'm actually running this thing, in-order doesn't force us to "commit" to our choices in certain cases. for example, consider using `and`. I don't have to look for the entire string all at once, I can look for part of it, find smtg, and then once I start looking for a new thing I can retroactively say, oh, you know, we want to do an "and" node here...
+    * when can we break?
+        * if we hit `[` or `]` when we didn't mean to, there's no going back, so just abort that
+        * we *can't* break if, say, we hit `--` before we hit `+++`, bc these actually cancel out. but I guess our program should know that there is no `+`, there's only `--+++`
+    * is a search more effective if we start at the symbol we want, and backtrack through the graph up to `S`? the goal is like, neutrality...
 '''
+
+rules = [
+    {'rule': "SP: DP VP", 'ops': ""},
+
+    {'rule': "DP: D'", 'ops': ""},
+    {'rule': "DP: DP Conj DP", 'ops': ">>"},
+    {'rule': "DP: Pronoun", 'ops': "<<<<"},
+    {'rule': "D': D NP", 'ops': ">>"},
+    {'rule': "D': NP", 'ops': ">>>"},
+
+    {'rule': "NP: N'", 'ops': ">"},
+    {'rule': "NP: NP Conj NP", 'ops': "]"},
+    {'rule': "N': AP N'", 'ops': "+"},
+    {'rule': "N': N' PP", 'ops': ""},
+    {'rule': "N': N PP", 'ops': ""},
+    {'rule': "N': N", 'ops': "<"},
+
+    # VP rules
+    {'rule': "VP: V'", 'ops': ">"},
+    {'rule': "VP: VP Conj VP", 'ops': "<<<<"},
+    {'rule': "V': V' PP", 'ops': ""},
+    {'rule': "V': V' AdvP", 'ops': ""},
+    {'rule': "V': AdvP V'", 'ops': ""},
+    {'rule': "V': TV DP", 'ops': ">"},
+    {'rule': "V': DTV DP DP", 'ops': ">"},
+    {'rule': "V': V", 'ops': ""},
+    #{'rule': "V': V Comp SP", ops: "?"},
+
+    # AdvP Rules
+    {'rule': "AdvP: Adv'", 'ops': ""},
+    {'rule': "AdvP: AdvP Conj AdvP", 'ops': ""},
+    {'rule': "Adv': Adv' Conj Adv'", 'ops': "++"},
+    {'rule': "Adv': AdvP Adv'", 'ops': "["},
+    {'rule': "Adv': Adv", 'ops': "-"},
+
+    # AP Rules
+    {'rule': "AP: A'", 'ops': "+"},
+    {'rule': "AP: AP Conj AP", 'ops': "------"}, #?
+    {'rule': "A': A' Conj A'", 'ops': "--"},
+    {'rule': "A': AdvP A'", 'ops': "-"},
+    {'rule': "A': A PP", 'ops': ""},
+    {'rule': "A': A", 'ops': "+"},
+
+    # PP Rules
+    {'rule': "PP: P'", 'ops': ">>"},
+    {'rule': "PP: PP Conj PP", 'ops': ">>>>"},
+    {'rule': "P': P' PP", 'ops': ""},
+    {'rule': "P': P DP", 'ops': ""},
+    #{'rule': "P': P", 'ops': ""}
+]
+
+'''
+everytime we see XP or X' we interpret that as a choice node
+so I think a graph is a dictionary; keys are either XP or X' and represent the "choice nodes", and values are a list/set of the actual nodes attached to them.
+
+'''
+import random
+
+is_head = lambda X: X and not (X.endswith("'") or (len(X) > 1 and X.endswith("P")))
+class Node:
+    def __init__(self, rule_dict):
+        self.rule_str = rule_dict['rule']
+        self.ops = rule_dict['ops']
+        self.l = None
+        self.r = None
+        self.third = None
+
+        # rule is either XP or X', children is what it connects to
+        rule, children = self.rule_str.split(":")
+        children = children.strip().split(" ")
+        # XP phrase
+        if rule.endswith("P") and len(children) == 1:
+            self.r = children[0]
+        # X' phrase
+        else:
+            self.l = children[0]
+            if len(children) > 1:
+                self.r = children[1]
+                self.third = children[2] if len(children) > 2 else None
+
+
+        self.l_head = is_head(self.l)
+        self.r_head = is_head(self.r)
+
+        self.rule = rule
+        self.words = bool(self.l_head) + bool(self.r_head)
+        #print(rule, ':', self.l, self.r, self.third, '\t', self.l_head, self.r_head)
+
+        # TODO later
+        self.frequency_cost_multiplier = 1
+
+    def __str__(self):
+        return f"{self.rule}: " + ' '.join([x for x in [self.l, self.r, self.third] if x])
+
+    def __repr__(self):
+        return self.__str__()
+
+class Graph:
+    def __init__(self, rule_array):
+        self.add_nodes(rule_array)
+
+    def add_nodes(self, rule_array):
+        '''
+        initializes a dictionary of nodes
+        the keys are "choice nodes", strings; the values are the nodes
+        '''
+        self.nodes = {}
+        for rule_dict in rule_array:
+            node = Node(rule_dict)
+            self.nodes[node.rule] = self.nodes.get(node.rule, set()).union(set([node]))
+
+    def random_traverse(self):
+        root = 'SP'
+        visited = {}
+        def helper(root, head=False, depth=0):
+            if root is None:
+                return
+            if head:
+                #print(root)
+                #input(' ' * depth + '**' + root)
+                #print(' ' * depth + '**' + root)
+                print(root, end=' ')
+                return
+
+            nodes = list(self.nodes[root])
+
+            #weights = [1 / visited.get(n.rule_str, 1) for n in nodes]
+            #node = random.choices(nodes, weights=weights)[0]
+
+            weights = [5 if 'Conj' not in n.rule_str else 1 for n in nodes]
+            node = random.choices(nodes, weights=weights)[0]
+            #node = random.choice(nodes)
+            if depth > 12:
+                for n in nodes:
+                    if n.l_head:
+                        node = n
+                        break
+            visited[node.rule_str] = visited.get(node.rule_str, 1) + 1
+
+            '''if 'Conj' in node.rule_str:
+                if random.random() < 0.85:
+                    node = random.choice(list(self.nodes[root]))'''
+            helper(node.l, node.l_head, depth + 1)
+            #print(node)
+            #input(' ' * depth + str(node))
+            #print(' ' * depth + str(node))
+            helper(node.r, node.r_head, depth + 1)
+            helper(node.third, depth=depth+1)
+
+        helper(root)
+
+
+
+G = Graph(rules)
+G.random_traverse()
